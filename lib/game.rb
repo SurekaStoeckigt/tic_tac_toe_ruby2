44258a73847769d889e0f65cce49c69b602817fb
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-04-13 18:15:34 +0100

    completed the game class

--- lib/game.rb
+++ lib/game.rb
@@ -13,5 +13,46 @@ module TicTacToe
       @current_player, @other_player = @other_player, @current_player
     end
 
+    def solicit_move
+      "#{current_player.name}: Enter a symbol and a number between 1 and 9"
+    end
+
+    def get_move(current_player_move = gets.chomp)
+      move_to_coordinate(current_player_move)
+    end
+
+    def game_over_message
+      return "#{current_player.name} won!" if board.game_over == :winner
+      return "The game ended in a draw!" if board.game_over == :draw
+    end
+
+    def play
+      puts "#{current_player.name} has been selected as the current player"
+      while true do
+        puts solicit_move
+        x, y = get_move
+        board.set_cell(x,y,current_player.symbol)
+        if board.game_over
+          puts game_over_message
+        else
+          switch_players
+        end
+      end
+    end
+
+    def move_to_coordinate(current_player_move)
+      mapping_array = { "1" => [0,0],
+                        "2" => [1,0],
+                        "3" => [2,0],
+                        "4" => [0,1],
+                        "5" => [1,1],
+                        "6" => [2,1],
+                        "7" => [0,2],
+                        "8" => [1,2],
+                        "9" => [2,2]
+      }
+      return mapping_array[current_player_move]
+    end
+
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -36,8 +36,58 @@ module TicTacToe
         game.switch_players
         expect(game.other_player).to eq(current_player)
       end
-
     end
+
+  context "#solicit_move" do
+
+  it "asks the current player to make a move" do
+    game = Game.new([player_1, player_2])
+    allow(game).to receive(:current_player) { player_1 }
+    expect(game.solicit_move).to eq("player_1: Enter a symbol and a number between 1 and 9")
+  end
+
+  it "displays the correct message" do
+    game = Game.new([player_1, player_2])
+    allow(game).to receive(:current_player) { player_2 }
+    expect(game.solicit_move).to eq("player_2: Enter a symbol and a number between 1 and 9")
+  end
+end
+
+  context "#get_move" do
+
+    it "maps a number 1 to location (0,0)" do
+      game = Game.new([player_1, player_2])
+      expect(game.get_move("1")).to eq([0,0])
+    end
+
+    it "maps a number 7 to location (0,2)" do
+      game = Game.new([player_1, player_2])
+      expect(game.get_move("7")).to eq([0,2])
+    end
+
+    it "should receive move_to_coordinate method" do
+      game = Game.new([player_1, player_2])
+      expect(game).to receive(:move_to_coordinate)
+      game.get_move("1")
+    end
+  end
+
+  context "#game_over_message" do
+
+    it "returns the correct message if there is a winner" do
+      game = Game.new([player_1, player_2])
+      allow(game).to receive(:current_player) { player_1 }
+      allow(game.board).to receive(:game_over) { :winner }
+      expect(game.game_over_message).to eq("player_1 won!")
+    end
+
+    it "returns the correct message if the game ends in a draw" do
+      game = Game.new([player_1, player_2])
+      allow(game).to receive(:current_player) { player_1 }
+      allow(game.board).to receive(:game_over) { :draw}
+      expect(game.game_over_message).to eq("The game ended in a draw!")
+    end
+  end
 
   end
 end
