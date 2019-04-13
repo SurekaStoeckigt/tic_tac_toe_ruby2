fb51776394f6550c10789a12c832b96fa8554662
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-04-13 17:40:08 +0100

    added method to switch players

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -19,7 +19,25 @@ module TicTacToe
         game = Game.new([player_2, player_1])
         expect(game.other_player).to eq(player_2)
       end
+    end
+
+    context "#switch_players" do
+
+      it "changes current player to other player" do
+        game = Game.new([player_1, player_2])
+        other_player = game.other_player
+        game.switch_players
+        expect(game.current_player).to eq(other_player)
+      end
+
+      it "changes other player to current player" do
+        game = Game.new([player_1, player_2])
+        current_player = game.current_player
+        game.switch_players
+        expect(game.other_player).to eq(current_player)
+      end
 
     end
+
   end
 end
